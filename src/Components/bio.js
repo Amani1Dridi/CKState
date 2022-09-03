2e1f3403631f78b76962d83a8160642162bb109e
Amani1Dridi <amanidrid1995@gmail.com>
2022-09-03 13:30:15 +0200

    first commit

---
+++ src/Components/bio.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Bio() {
+    return (
+        <div>
+            <h1>Super Women </h1>
+        </div>
+    )
+}
+
+export default Bio
---
+++ src/Components/img.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import amani from '../public/me.jpg'
+function ImgSrc() {
+    return (
+        <div>
+            <img src={amani} alt="myImg"/>
+        </div>
+    )
+}
+
+export default ImgSrc
